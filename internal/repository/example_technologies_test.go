87f6da827e5d83298219f48aad9d6d8c16b5e3fd
skvdmt <skvdmt@yandex.ru>
2026-09-12 09:58:49 +0300

    fixed

--- internal/repository/example_technologies_test.go
+++ internal/repository/example_technologies_test.go
@@ -33,7 +33,7 @@ func TestExampleTechnologies(t *testing.T) {
 	}
 
 	expecteds := []string{
-		"Go", "Docker", "Git", "Vue2",
+		"Go", "Docker", "Git", "Vue",
 	}
 	t.Run(fmt.Sprintf("example %s technologies", title), func(t *testing.T) {
 		actuals, err := p.app.exampleTechnologies(context.Background(), *id)
